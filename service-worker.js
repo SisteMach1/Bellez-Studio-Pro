5713f8405d780325eb87797759592d2293914240
SisteMach <maxiisanchezz2020@gmail.com>
2026-09-06 23:35:59 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='belleza-pro-v3-limpia';
+const CACHE='belleza-v4-ultra-limpia';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])));self.skipWaiting();});
 self.addEventListener('activate',e=>{self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)).catch(()=>caches.match('./index.html')));});
